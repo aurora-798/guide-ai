5f6c84617520586d96d8670ffaea815eeb3fe991
changge <3415181520@qq.com>
2025-11-30 11:45:49 +0800

    后端项目初始化完成

---
+++ src/main/java/com/guide/aiguide/AiGuideApplication.java
@@ -0,0 +1,13 @@
+package com.guide.aiguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiGuideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/guide/aiguide/AiGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guide.aiguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
